b992024e380ff83e5264fcba6960eb5766be98df
Yo Mizuki <ymizuki0530@gmail.com>
2024-04-01 12:13:48 +0900

    Update MainPage Output Item Contact List

--- src/main/java/com/example/app/domain/User.java
+++ src/main/java/com/example/app/domain/User.java
@@ -1,5 +1,6 @@
 package com.example.app.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 public class User {
 	private Integer userId;
 	private String userName;
+	@NotBlank
 	private String userLogin;
+	@NotBlank
 	private String userPassword;
 }
